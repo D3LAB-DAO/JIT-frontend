83b2737cf2574a6f5004ef15b2fbdf3dd890ea6e
clovermini22 <clovermini22@gmail.com>
2022-05-21 22:11:17 +0900

    Connect Metamask & Add Wallet Page

--- frontend/public/js/interface.js
+++ frontend/public/js/interface.js
@@ -1,6 +1,9 @@
 /* Constants */
 // Page Types
-const PAGES = ['home', 'top', 'people', 'lend', 'mint', 'govern'];
+const PAGES = ['home', 'top', 'people', 'lend', 'mint', 'govern', 'wallet'];
+const LEND_PAGE = 3;
+const MINT_PAGE = 4;
+const WALLET_PAGE = 6;
 
 /* Saved Values on Frontend */
 let current_page = 0;
@@ -9,16 +12,39 @@ let try_page = -1;
 /*
  * Onclick functions
  */
+async function connectOnClick() {
+    if (account !== '') return;
+
+    // connect to metamask
+    let success = await connectMetamask();
+
+    if (success) {
+        // set elements
+        $('.connect-btn-text').html(account.substr(0, 4) + '..' + account.substr(account.length - 2, 2));
+        $('.connect-btn-text-sm').html(account.substr(0, 4));
+        $('.connect-btn').attr('uk-tooltip', 'title: ' + account);
+
+        // update elements by request
+
+        // update try page
+        if (try_page != -1)
+            movePageOnClick(try_page);
+    }
+    else {
+        console.log("metamask connection failed!");
+    }
+}
+
 function movePageOnClick(targetIdx) {
     if (targetIdx == current_page) return;
-    /*
-    if (account == '' && targetIdx != START_PAGE) {
+    if (targetIdx == try_page) try_page = -1;
+
+    // WALLET PAGE FEATURE
+    if (account == '' && (targetIdx == LEND_PAGE || targetIdx == MINT_PAGE)) {
         try_page = targetIdx;
         targetIdx = WALLET_PAGE;
     }
-
-    updatePageOnClick(targetIdx);
-    */
+    
     $('#page-' + PAGES[current_page]).hide();
     $('#page-' + PAGES[targetIdx]).fadeIn(500);
 
